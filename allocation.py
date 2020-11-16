b95c2ce46b53fdec3a28bad06cf14d7e552cb9c7
Nick Sylva <nick.sylva@protonmail.com>
2020-11-16 11:16:24 -0800

    abstracted out model references to pool references

--- allocation.py
+++ allocation.py
@@ -7,53 +7,53 @@ import warnings
 class Allocator:
     '''
     Class that acts as a mechanism to allocate targeting predictions
-    from multiple models with options for different picking strategies.
+    from multiple pools with options for different picking strategies.
     Configuration parameters:
-        - allocation distribution (dict): keys are model ids (str) and values 
+        - allocation distribution (dict): keys are pool ids (str) and values 
                                           are the proportion of targets to 
                                           allocate (float). Sum of values must
                                           add up to 1.0.
         - num_allocations (int): number of vb_voterbase_ids to allocate across
-                                 the models. If this number is greater than the
+                                 the pools. If this number is greater than the
                                  number of ids in the predictions dataframe, the
                                  maximum number of ids in the predictions
                                  dataframe will be used instead.
         - predictions (pd.DataFrame): Pandas DataFrame that contains the
-                                      predictions for each model. At a minimum,
+                                      predictions for each pool. At a minimum,
                                       this object must have columns:
                                          - vb_voterbase_id (any type)
-                                         - model_id (str matching model_ids in
+                                         - pool_id (str matching pool_ids in
                                                     allocation_distribution)
                                          - probability (float)
-                                      If your model only predicts classes and
+                                      If your pool only predicts classes and
                                       not probabilities, use 1 for the 
                                       probability column.
         - strategy (str) [optional]: Strategy for how predictions are picked
                                      from the targeting allocation. Values:
                                         - round-robin [default]: starts with 
-                                          the model with the least allocation
+                                          the pool with the least allocation
                                           and snakes through the sequence of 
-                                          models until num_allocations reached.
+                                          pools until num_allocations reached.
                                         - greedy: picks predictions from the 
-                                          model with the highest allocation 
-                                          until that model has no more 
+                                          pool with the highest allocation 
+                                          until that pool has no more 
                                           allocations left, then from the next
-                                          model in decreasing order.
+                                          pool in decreasing order.
                                         - altruist: picks predictions from the 
-                                          model with the least allocation until
-                                          that model has no more allocations
-                                          left, then from the next model in 
+                                          pool with the least allocation until
+                                          that pool has no more allocations
+                                          left, then from the next pool in 
                                           increasing order.
         - order (str) [optional]: Order dictating which prediction should be
-                                  picked from a given model. Values:
+                                  picked from a given pool. Values:
                                     - random: picks a random prediction from 
-                                      the model.
+                                      the pool.
                                     - best: picks the prediction with the
-                                      highest probability from the model.
+                                      highest probability from the pool.
                                     - worst: picks the prediction with the
-                                      lowest probability from the model.
+                                      lowest probability from the pool.
     Usage: Object is configured on instantiation. To extract a list of ids
-           allocated by the model, call the allocate_predictions method. This
+           allocated by the pool, call the allocate_predictions method. This
            method has output that can be assigned to a variable or it can be
            ran in place. If ran in place, access the allocations with the 
            .allocations property.
@@ -63,6 +63,9 @@ class Allocator:
                  allocation_distribution,
                  num_allocations,
                  pool,
+                 pool_id_col,
+                 score_col,
+                 target_id_col,
                  strategy = 'round-robin',
                  order = 'random'):
         self._num_allocations = None
@@ -71,6 +74,9 @@ class Allocator:
         self.allocation_distribution = allocation_distribution
         self._pool = None
         self.pool = pool
+        self.pool_id_col = pool_id_col
+        self.score_col = score_col
+        self.target_id_col = target_id_col
         self._strategy = None
         self.strategy = strategy
         self._order = None
@@ -94,11 +100,11 @@ class Allocator:
             raise TypeError('At least one allocation in allocation_distribution'
                             ' is not a float. Verify supplied allocations.')
         
-        sum_of_model_allocations = sum([v for v in allocation_distribution.values()])
+        sum_of_pool_allocations = sum([v for v in allocation_distribution.values()])
         
-        if sum_of_model_allocations != 1.0:
+        if sum_of_pool_allocations != 1.0:
             raise ValueError('Sum of pool allocations must equal 1.0 not '
-                             f'{sum_of_model_allocations}.')            
+                             f'{sum_of_pool_allocations}.')            
         else:
             self._allocation_distribution = allocation_distribution
 
@@ -111,14 +117,14 @@ class Allocator:
         '''
         Validates the pool passed to the constructor or any updated
         pool after initialization. Sets up the "picked" variable and 
-        sorts the DataFrame by model_id and probability.
+        sorts the DataFrame by pool_id_col and score.
         '''
         if not isinstance(obj,pd.DataFrame):
             raise TypeError(f'pool must be a pd.DataFrame, not {type(obj)}')
         else:
             self._pool = obj
             self._pool['picked'] = 0
-            self._pool.sort_values(['model_id','probability'],
+            self._pool.sort_values([self.pool_id_col,self.score_col],
                                            ascending=False,
                                            inplace=True)
 
@@ -186,41 +192,41 @@ class Allocator:
             # case when worst pred is chosen first
             return indices[-1]
 
-    def pick_id(self,model_id):
+    def pick_id(self,pool_id):
         '''
-        For a given model, picks an index and extracts the vb_voterase_id at
+        For a given pool, picks an index and extracts the vb_voterase_id at
         that index.
         '''
-        # get a pred id from the model per to order strategy
-        indices = self.pool[(self.pool['model_id'] == model_id)
+        # get a pred id from the pool per to order strategy
+        indices = self.pool[(self.pool[self.pool_id_col] == pool_id)
                                     & (self.pool['picked'] == 0)].index
         idx = self.pick_index(indices)
-        vid = self.pool.at[idx,'vb_voterbase_id']
+        vid = self.pool.at[idx,self.target_id_col]
         return idx, vid
 
-    def allocate_id(self,index,voter_id,model_id):
+    def allocate_id(self,index,target_id,pool_id):
         '''
         Adds the given vb_voterbase_id to the allocations and updates all rows
         with that id to indicate it has been picked already.
         '''
         #allocate the ID
-        self.allocations.append((voter_id,model_id))
+        self.allocations.append((target_id,pool_id))
         # set picked column for that id
-        id_indices = self.pool[self.pool['vb_voterbase_id'] == voter_id].index
+        id_indices = self.pool[self.pool[self.target_id_col] == target_id].index
         for i in id_indices:
             self.pool.at[i,'picked'] = 1
 
-    def make_n_allocations_from_model(self,n,model_id):
+    def make_n_allocations_from_pool(self,n,pool_id):
         '''
-        Wrapper to make multiple allocations for a model at once.
+        Wrapper to make multiple allocations for a pool at once.
         '''
         if not n >= 1:
             raise ValueError('Number of allocations to make is invalid. \
                               Must be >= 1')
         made = 0
         while made < n:
-            idx, v_id = self.pick_id(model_id)
-            self.allocate_id(idx,v_id,model_id)
+            idx, v_id = self.pick_id(pool_id)
+            self.allocate_id(idx,v_id,pool_id)
             made += 1
 
     def allocate_pool(self):
@@ -231,8 +237,8 @@ class Allocator:
         instance.
         Also returns self.allocations for direct access when called.
         '''
-        num_models = len(self.pool['model_id'].unique())
-        total_preds = self.pool.shape[0]//num_models
+        num_pools = len(self.pool[self.pool_id_col].unique())
+        total_preds = self.pool.shape[0]//num_pools
         if total_preds < self.num_allocations:
             warnings.warn('Specified number of allocations, '
                           f'{self.num_allocations}, is greater than the '
@@ -241,46 +247,46 @@ class Allocator:
         else:
             total_preds = self.num_allocations
 
-        # generate the number of pool to make for each model
-        models = []
-        for model,allocation in self.allocation_distribution.items():
-            models.append({'id':model,
+        # generate the number of pool to make for each pool
+        pools = []
+        for pool,allocation in self.allocation_distribution.items():
+            pools.append({'id':pool,
                            'count':ceil(total_preds*allocation),
                            'allocated':0})
 
-        # generate order of model choice, model with fewest preds goes first
-        models.sort(key=lambda x:x['count'])
+        # generate order of pool choice, pool with fewest preds goes first
+        pools.sort(key=lambda x:x['count'])
 
         if self._strategy == 'round-robin':
             allocated = 0
-            current_model_index = 0
+            current_pool_index = 0
             while allocated < total_preds:
-                model = models[current_model_index]
-                if model['allocated'] < model['count']:
-                    self.make_n_allocations_from_model(1,model['id'])
+                pool = pools[current_pool_index]
+                if pool['allocated'] < pool['count']:
+                    self.make_n_allocations_from_pool(1,pool['id'])
                     allocated += 1
-                    model['allocated'] += 1
+                    pool['allocated'] += 1
                 # because we are going in round-robin order, reverse the next time
-                if current_model_index == num_models - 1:
-                    models.reverse()
-                    current_model_index = 0
+                if current_pool_index == num_pools - 1:
+                    pools.reverse()
+                    current_pool_index = 0
                 else:
-                    current_model_index += 1
+                    current_pool_index += 1
         elif self._strategy == 'greedy':
-            # greedy strat, pick from model with greatest allocation first
+            # greedy strat, pick from pool with greatest allocation first
             remaining_allocations = total_preds - len(self.allocations)
-            for model in reversed(models):
-                n = min(model['count'],remaining_allocations)
+            for pool in reversed(pools):
+                n = min(pool['count'],remaining_allocations)
                 if n >= 1:
-                    self.make_n_allocations_from_model(n,model['id'])
+                    self.make_n_allocations_from_pool(n,pool['id'])
                     remaining_allocations -= n
         else:
-            # altruist strat, pick from model with smallest allocation first
+            # altruist strat, pick from pool with smallest allocation first
             remaining_allocations = total_preds - len(self.allocations)
-            for model in models:
-                n = min(model['count'],remaining_allocations)
+            for pool in pools:
+                n = min(pool['count'],remaining_allocations)
                 if n >= 1:
-                    self.make_n_allocations_from_model(n,model['id'])
+                    self.make_n_allocations_from_pool(n,pool['id'])
                     remaining_allocations -= n
 
         return self.allocations
