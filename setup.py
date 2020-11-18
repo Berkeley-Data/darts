bd8a9ebbbfd663e5c04e5de64a679743fdcc8fb4
Nick Sylva <nick.sylva@protonmail.com>
2020-11-18 13:39:30 -0800

    add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,20 @@ setup(
     version='0.0.1',
     description='Dynamic and responsive targeting system using multi-arm bandits modified for delayed feedback.',
     py_modules=['bandit','allocation'],
-    package_dir={'': 'src'}
+    package_dir={'': 'src'},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
 )
